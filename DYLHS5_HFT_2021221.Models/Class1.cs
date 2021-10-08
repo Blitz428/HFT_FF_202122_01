d5701655a0ac550cd029ab6b57863caf4255c99c
Szegedi Tamás <szegedi.tamas@stud.uni-obuda.hu>
2021-10-08 13:53:34 +0200

    Required NuGet packages have been downloaded. Database .mdl, .ldf files are created. Automatically created "Class1.cs" files have been deleted.

--- DYLHS5_HFT_2021221.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DYLHS5_HFT_2021221.Models
-{
-    public class Class1
-    {
-    }
-}
--- DYLHS5_HFT_2021221.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DYLHS5_HFT_2021221.Repository
-{
-    public class Class1
-    {
-    }
-}
