98b9dbe0ae7df552ce078849d5a7849d39e72c76
Szegedi Tamás <szegedi.tamas@stud.uni-obuda.hu>
2021-11-22 18:32:45 +0100

    Test classes and testcases created

---
+++ DYLHS5_HFT_2021221.Test/OrderLogicTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DYLHS5_HFT_2021221.Logic;
+using DYLHS5_HFT_2021221.Models;
+using DYLHS5_HFT_2021221.Repository;
+using Moq;
+using NUnit.Framework;
+
+namespace DYLHS5_HFT_2021221.Test
+{
+    [TestFixture]
+    class OrderLogicTests
+    {
+    }
+}
